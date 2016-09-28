b193e8650bfe6d56492e01b1f2285ff8b382ef77
Learn Devops <learndevops007@gmail.com>
2016-09-28 08:00:01 -0700

    spark central challenge

--- Calulcator.py
+++ Calulcator.py
@@ -1,51 +1,52 @@
-class Calculator():
+class Calci():
     def __init__(self, tokens):
         self._tokens = tokens
         self._current = tokens[0]
     
-    def exp(self):
-        result = self.term()
+    def expression(self):
+        eq_result = self.term()
         while self._current is '+':
             self.next()
-            result += self.term()
+            eq_result += self.term()
             print(result)
         while self._current is '-':
             self.next()
-            result -= self.term()
-        return result
+            eq_result -= self.term()
+        return eq_result
 
     def factor(self):
-        result = None
+        eq_result = None
         if self._current.isdigit():
-            result = float(self._current)
+            eq_result = float(self._current)
             self.next()
         elif self._current is '(':
             self.next()
-            result = self.exp()
+            eq_result = self.exp()
             self.next()
-        return result
+        return eq_result
 
     def next(self): 
         self._tokens = self._tokens[1:]
         self._current = self._tokens[0] if len(self._tokens) > 0 else None
 
     def term(self):
-        result = self.factor()
+        eq_result = self.factor()
         while self._current is '*':
             self.next()
-            result *= self.factor()
+            eq_result *= self.factor()
         while self._current is '/':
             self.next()
-            result /= self.factor()
-        return result
+            eq_result /= self.factor()
+        return eq_result
 
 if __name__ == '__main__':
     while True:
-        lst = list(input('> '))
+        print("please enter your calculation expression")
+        my_list = list(input('> '))
         tokens = []
-        for i in range(len(lst)):
-            if lst[i].isdigit() and len(tokens) > 0 and tokens[-1].isdigit():
-                tokens[-1] += lst[i]
+        for i in range(len(my_list)):
+            if my_list[i].isdigit() and len(tokens) > 0 and tokens[-1].isdigit():
+                tokens[-1] += my_list[i]
             else:
-                tokens.append(lst[i])
-        print (Calculator(tokens).exp() )
+                tokens.append(my_list[i])
+        print (Calci(tokens).expression() )
